0005debc79799d7dec0e50c325203e32ce924fd8
aarontsamaltoupis-singleton <aaron.tsamaltoupis@gmail.com>
2025-11-15 10:47:45 +0100

    letzter speicherpunkt praktikum2

--- eoom/praktikum/praktikum2/implementierung/PythagoraeischeZahltripelMain.java
+++ eoom/praktikum/praktikum2/implementierung/PythagoraeischeZahltripelMain.java
@@ -1,4 +1,4 @@
-public class Main {
+public class PythagoraeischeZahltripelMain{
     public static void main(String[] args) {
         PythagoraeischeZahlentripel pyth = new PythagoraeischeZahlentripel(1, 100);
         pyth.gibtAnzahlAus();
