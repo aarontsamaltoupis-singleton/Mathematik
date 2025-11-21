7176e0976d87d467b9d3804cdb7f60076e525eb9
Your Name <you@example.com>
2025-11-21 22:45:53 +0100

    programme reiehen

--- Analysis1/programme/versuch.py
+++ Analysis1/programme/versuch.py
@@ -1,13 +1,37 @@
-n = 100
+# folge aller zahlen ohne 9
+def c(n):
+    c = []
+    f = []
+    x = 1
+    while len(c)<n:
+        if str(9) not in str(x):
+            c.append(x)
+                
+            
+        x+=1
+    
+    return c
 
-m = 0
-for k in range(1,n):
-	m += k(1/(k**2)-1/((k+1)**2))
+# anzahl aller zahlen ohne 9 zwischen 1 und n
+def f(n):
+    f = []
+    for x in range(1,n+1):
+        if str(9) not in str(x):
+            f.append(x)
 
-m2 = 0
-for k in range(1,n):
-	m2 += 1/k**2
+    return len(f)
+    
 
 
-	
+# folge aller zahlen
+def id(n):
+    return list(range(n))
 
+i = 5
+n = 10**i
+#print(c(n)[-1])
+#print(f(n))
+#print(n*n/f(n))
+
+for n in range(1,1000):
+    print(10**n/9**n-n**2)
