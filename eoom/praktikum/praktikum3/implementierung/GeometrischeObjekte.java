fe86e6fed8c675809e4bed25fb593cb3263824bc
aarontsamaltoupis-singleton <aaron.tsamaltoupis@gmail.com>
2025-12-03 08:46:01 +0100

    prak. 3

---
+++ eoom/praktikum/praktikum3/implementierung/GeometrischeObjekte.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public class GeometrischeObjekte {
+    ArrayList<GeometrischesObjekt> geometrischeObjekte = new ArrayList<>();
+    GeometrischeObjekte(){
+    }
+    public void add(GeometrischeObjekt neuesElement){
+        geometrischeObjekte.add(neuesElement);
+
+    public double berechneGesamtesVolumen(){
+        double summe;
+        for(objekt: geometrischeObjekte, )
+    }
+
+    }
+    
+
+
+
+}
