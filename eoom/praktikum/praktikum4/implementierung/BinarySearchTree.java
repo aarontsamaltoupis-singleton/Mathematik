4f1b91ef7c1b5b8bc2f34c025a80ba2593e7220a
Your Name <you@example.com>
2025-12-09 17:26:54 +0100

    d

---
+++ eoom/praktikum/praktikum4/implementierung/BinarySearchTree.java
@@ -0,0 +1,11 @@
+public class BinarySearchTree {
+	private final BinarySearchTreeNode root = new BinarySearchTreeNode();
+
+	public BinarySearchTree() {
+	}
+
+	public void add(final int number) {
+		root.add(number);
+	}
+
+}
